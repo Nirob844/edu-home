e6d6bb4a720c2a6a2e9b7d0cb726eb0acb7cc8fd
Nirob Hasan <nirob427417@gmail.com>
2023-01-28 11:28:10 +0600

    routes setup

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAbFhv61JXkhpdiB6p4XmojmiVb48J6xG8",
+    authDomain: "edu-home-da2e3.firebaseapp.com",
+    projectId: "edu-home-da2e3",
+    storageBucket: "edu-home-da2e3.appspot.com",
+    messagingSenderId: "955784417854",
+    appId: "1:955784417854:web:021fb44223f8876937f76f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
